1ef65c76d14a155b966d1120cea0d8c3f3e12180
Sedmikras <kouba.prem@gmail.com>
2016-10-19 22:06:59 +0200

    vytvořen zip se semestrální prací k odeslání

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#! \bin\bash
+#!/bin/bash
 	# Skript pro sestavení semestrální práce z Teoretické Informatiky
 javac -d . -encoding UTF-8  src/TeoreticalInformatics/*.java
 echo Main-Class: TeoreticalInformatics.Main > man.txt
