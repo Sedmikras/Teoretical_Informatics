6239100e0fd16e33e0c0cb1e5fef60744511abac
Sedmikras <kouba.prem@gmail.com>
2016-10-19 21:37:16 +0200

    oprava překlepů a drobné úpravy kódu

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! \bin\bash
+	# Skript pro sestavení semestrální práce z Teoretické Informatiky
+javac -d . -encoding UTF-8  src/TeoreticalInformatics/*.java
+echo Main-Class: TeoreticalInformatics.Main > man.txt
+jar cmf man.txt TI.jar TeoreticalInformatics src
